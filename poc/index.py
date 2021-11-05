25b3b6c49a088eddaa64fefb09eb7ae3283ae23d
einzbernnn <77831842+einzbernnn@users.noreply.github.com>
2021-11-05 10:16:26 +0800

    Delete poc directory

--- poc/index.py
+++
@@ -1,6 +0,0 @@
-from poc import cve_2017_12615
-from poc import cve_2017_12617
-from poc import cve_2019_0232
-from poc import cve_2020_1938
-
-pocindex=['cve_2017_12615','cve_2017_12617','cve_2019_0232','cve_2020_1938']
